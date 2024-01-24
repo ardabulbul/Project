03e6b41f726545088d8db71804e751ce1114d327
ardabulbul <ardaberkaybulbul@gmail.com>
2024-01-25 00:14:34 +0300

    Urun detay

--- second/index.js
+++ second/index.js
@@ -3,12 +3,20 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+const data = [
+  { id: 1, name: "iphone 14", price: 30000, isActive: true },
+  { id: 2, name: "iphone 15", price: 40000, isActive: false },
+  { id: 3, name: "iphone 16", price: 50000, isActive: true },
+];
+
 app.use("/urunler/:id", function (req, res) {
   res.render("urun-details", { id: req.params.id });
 });
 
 app.use("/urunler", function (req, res) {
-  res.render("urunler");
+  res.render("urunler", {
+    liste: data,
+  });
 });
 
 app.use("/", function (req, res) {
