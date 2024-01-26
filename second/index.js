0b527dcbd4eba4eb1a1676c4e0d653c52650fbe1
ardabulbul <ardaberkaybulbul@gmail.com>
2024-01-26 23:37:08 +0300

    Dinamik

--- second/index.js
+++ second/index.js
@@ -5,12 +5,17 @@ app.set("view engine", "ejs");
 
 const data = [
   { id: 1, name: "iphone 14", price: 30000, isActive: true },
-  { id: 2, name: "iphone 15", price: 40000, isActive: false },
+  { id: 2, name: "iphone 15", price: 40000, isActive: true },
   { id: 3, name: "iphone 16", price: 50000, isActive: true },
 ];
 
 app.use("/urunler/:id", function (req, res) {
-  res.render("urun-details", { id: req.params.id });
+  const urun = data.find((u) => u.id == req.params.id);
+  res.render("urun-details", {
+    name: urun.name,
+    price: urun.price,
+    isActive: urun.isActive,
+  });
 });
 
 app.use("/urunler", function (req, res) {
@@ -24,5 +29,5 @@ app.use("/", function (req, res) {
 });
 
 app.listen(3000, () => {
-  console.log("listening on port 3000");
+  console.log("Port 3000 üzerinde dinleniyor");
 });
