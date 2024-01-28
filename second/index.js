6e8d65bbc60587bf18364d44e7464ff26ce820b0
ardabulbul <ardaberkaybulbul@gmail.com>
2024-01-28 20:35:20 +0300

    bootstrap

--- second/index.js
+++ second/index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "node_modules")));
 
 const data = [
   {
