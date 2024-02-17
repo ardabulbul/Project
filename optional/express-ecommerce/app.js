f5d40220a748e49500463160cd5c05d56543adc5
ardabulbul <ardaberkaybulbul@gmail.com>
2024-02-17 20:12:20 +0300

    ecommerce

---
+++ optional/express-ecommerce/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const indexRouter = require("./routes/index");
+const productsRouter = require("./routes/products");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/products", productsRouter);
+
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
